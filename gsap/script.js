6dd06216455f2921c4a7b3528f63ee54dc8eebe5
Da hee <ekdudekgml35@naver.com>
2022-05-16 14:47:58 +0900

    ease case 살펴보기

--- gsap/script.js
+++ gsap/script.js
@@ -1,3 +1,22 @@
-gsap.to(".box", {
-    x: 200
-  });
+gsap.to(".box1", {
+    duration: 3,
+    x: 200,
+    opacity: 0.8,
+    ease: 'back(7.0)', y: 100,
+});
+
+gsap.to(".box2", {
+    duration: 3,
+    x: 200,
+    rotate: 720,
+    scale: 1.3
+});
+
+gsap.to(".box3", {
+    duration: 3,
+    x: 200,
+    ease: 'elastic',
+    backgroundColor: 'red',
+    width: 300,
+    fontSize: 60 
+});
